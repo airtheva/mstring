297e69568208095878eab47bc3e3c1e7379577fe
JESSIZUN AIRTHEVA <airtheva@foxmail.com>
2013-08-22 21:44:43 +0800

    Make it more flexible and work better in Windows.

--- mstring.js
+++ mstring.js
@@ -1,26 +1,26 @@
 /* Copyright (c) 2012 Richard Rodger */
 
-var errmsg = "mstring: required format is function (){/*** ... ***/}, this is invalid: "
+var errmsg = "mstring: required format is function (){/*** ... ***/}, this is invalid: ";
 
 module.exports = function(f){
   if( !_.isFunction(f) ) {
-    throw new Error(errmsg+f)
+    throw new Error(errmsg+f);
   }
 
-  var fs = f.toString()
-  var m  = fs.match(/^function\s*\(\)\s*\{\s*\/\*\*\*\n([\s\S]*)\n\*\*\*\/\s*\}$/) 
+  var fs = f.toString();
+  // Modified by airtheva@github, making it more flexible and work better in Windows.
+  //var m  = fs.match(/^function\s*\(\)\s*\{\s*\/\*\*\*\n([\s\S]*)\n\*\*\*\/\s*\}$/);
+  var m  = fs.match(/^function\s*\(\)\s*\{\s*\/\*\*\*\r?\n?([\s\S]*)\r?\n?\s*\*\*\*\/\s*\}$/);
   if( m && _.isString(m[1]) ) {
-    return m[1]
+    return m[1];
   }
-  else throw new Error(errmsg+f);
+  else throw new Error(errmsg+fs);
 }
 
-
-var _ = {}
+var _ = {};
 _.isFunction = function(obj) {
   return !!(obj && obj.constructor && obj.call && obj.apply);
 };
 _.isString = function(obj) {
   return !!(obj === '' || (obj && obj.charCodeAt && obj.substr));
 };
-
